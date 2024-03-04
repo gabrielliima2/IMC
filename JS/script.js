9deaeb40eba3719224b707acf4774bacf35321d1
Gabriel lima <121819533+gabrielliima2@users.noreply.github.com>
2024-03-04 18:18:17 -0300

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,7 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-form.onsubmit = event => {
+form.onclick = event => {
     event.preventDefault()
 
     const weight = inputWeight.value
